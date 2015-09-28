aa5806b0d909966866d48962b30c2a84861f3d40
Ted Whang <ted@quve.com>
2015-09-27 18:06:37 -0700

    Updated navbar background color to always be white

--- app/assets/javascripts/root.js
+++
@@ -1,7 +0,0 @@
-$(document).scroll(function() {
-  if ($(window).scrollTop() > 100) {
-    $('.navbar').addClass('scroll-nav-bar', { duration: 1000 });
-  } else {
-    $('.navbar').removeClass('scroll-nav-bar', { duration: 1000 });
-  }
-});
