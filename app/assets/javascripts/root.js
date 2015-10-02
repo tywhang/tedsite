6090db4dc6e22f645cfe5f7eda38909e53d48ea6
Ted Whang <ted@quve.com>
2015-10-01 19:22:47 -0700

    Modal closes upon form submit

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -1,15 +1,22 @@
-$('.js-send-email').click(function() {
-  var senderName = $('.js-sender-name').val();
-  var senderEmail = $('.js-sender-email').val();
-  var body = $('.js-email-body').val();
-  
-  $.ajax({
-    url: '/root/contact_me',
-    type: 'POST',
-    data: {
-      sender_name: senderName,
-      sender_email: senderEmail,
-      body: body
-    }
+$(document).ready(function() {
+  $('.js-send-email').click(function() {
+
+    var senderName = $('.js-sender-name').val();
+    var senderEmail = $('.js-sender-email').val();
+    var body = $('.js-email-body').val();
+
+    $.ajax({
+      url: '/root/contact_me',
+      type: 'POST',
+      data: {
+        sender_name: senderName,
+        sender_email: senderEmail,
+        body: body
+      }
+    }).success(function() {
+      debugger;
+      jQuery.noConflict();
+      $('#contact-me-form').modal('hide');
+    });
   });
 });
