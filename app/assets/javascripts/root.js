d8218e042cae98f6e83c2779bc4c0c337fcdf4c4
Ted Whang <ted@quve.com>
2015-10-01 20:22:17 -0700

    Validate presence of form fields and added underscorejs

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
   $('.js-send-email').click(function() {
+    var senderName = $('.js-sender-name');
+    var senderEmail = $('.js-sender-email');
+    var body = $('.js-email-body');
 
-    var senderName = $('.js-sender-name').val();
-    var senderEmail = $('.js-sender-email').val();
-    var body = $('.js-email-body').val();
+    var isFormValid = true;
+    _.each([senderName, senderEmail, body], function(input) {
+      if (s.isBlank(s.trim(input.val()))) {
+        input.after("<p class='error'>This field is required</p>");
+        isFormValid = false;
+      }
+      $('.error').fadeOut({ duration: 2000 });
+    });
+
+    if (!isFormValid) { return; }
 
     $.ajax({
       url: '/root/contact_me',
       type: 'POST',
       data: {
-        sender_name: senderName,
-        sender_email: senderEmail,
-        body: body
+        sender_name: senderName.val(),
+        sender_email: senderEmail.val(),
+        body: body.val()
       }
     }).success(function() {
       jQuery.noConflict();
