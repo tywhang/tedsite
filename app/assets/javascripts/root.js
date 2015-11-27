11cac273e18dafdcdf523111f95b515538b322cf
Ted <taeywhang@gmail.com>
2015-11-27 11:59:49 -0800

    Navbar background

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > ($('.header-image').height()) - $('.js-navbar').height() ) {
+      $('.js-navbar').addClass('black-background');
+    } else {
+      $('.js-navbar').removeClass('black-background');
+    }
+  });
+
+
   $('.js-send-email').click(function() {
     var senderName = $('.js-sender-name');
     var senderEmail = $('.js-sender-email');
