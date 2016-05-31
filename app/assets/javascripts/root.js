c13c762ddb613395da1ae494ed9627f5012d3e70
Ted <taeywhang@gmail.com>
2016-05-30 21:44:40 -0700

    Get rid of overlays

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -7,24 +7,4 @@ $(document).ready(function() {
       $('.js-navbar').removeClass('black-background');
     }
   });
-
-  $('.js-show-overlay--mazlo').click(function() {
-    $('#main').addClass('mazlo-overlay-open');
-  });
-
-  $('.js-show-overlay--ralli').click(function() {
-    $('#main').addClass('ralli-overlay-open');
-  });
-
-  $('.js-show-overlay--muzik').click(function() {
-    $('#main').addClass('muzik-overlay-open');
-  });
-
-  $('.js-close-overlay, .overlay').click(function() {
-    $('#main').removeClass('mazlo-overlay-open ralli-overlay-open muzik-overlay-open')
-  });
-
-  $('.overlay__content').click(function(e) {
-    e.stopPropagation();
-  });
 });
